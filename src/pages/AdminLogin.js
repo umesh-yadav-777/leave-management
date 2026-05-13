d89e0c8f439248834154b59b46e2e93c10f8b928
umesh-yadav-777 <yaduvanshiumesh045@gmail.com>
2026-05-13 23:04:04 +0530

    Update AdminLogin.js

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../components/api"; // Path check kar lena
+import API from "../components/api"; 
 
 function AdminLogin() {
   const [email, setEmail] = useState("");
@@ -10,18 +10,16 @@ function AdminLogin() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      // Backend ko 'username' key mein email bhejna hai
       const response = await API.post("accounts/login/", {
         username: email,
         password: password
       });
 
-      // Role check
       if (response.data.role === "admin") {
         localStorage.setItem("access_token", response.data.access);
         localStorage.setItem("userRole", "admin");
         alert("Admin Login Successful!");
-        navigate("/admin-dashboard"); // Dashboard ka sahi path yahan likhein
+        navigate("/admin-dashboard");
       } else {
         alert("Aap Admin nahi hain! Normal login use karein.");
       }
